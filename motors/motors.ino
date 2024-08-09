bd926f371994dbc7ba189d7ba12e5b75850521b8
Jakub Wilk <kubawilk63@gmail.com>
2024-08-09 18:02:33 +0200

    Correct motor direction

--- motors/motors.ino
+++ motors/motors.ino
@@ -1,4 +1,4 @@
-int In1 = 4; //set L298n PIN
+int In1 = 4; // set L298n PIN
 int In2 = 5;
 int In3 = 6;
 int In4 = 7;
@@ -6,7 +6,10 @@ int In4 = 7;
 int enA = 8;
 int enB = 9;
 
-void setup() {
+int distance_threshold = 10; // Change the value here
+bool box_detected = false;
+void setup()
+{
   pinMode(In1, OUTPUT); // Arduino control a car using output voltage
   pinMode(In2, OUTPUT);
   pinMode(In3, OUTPUT);
@@ -16,17 +19,35 @@ void setup() {
   pinMode(enA, OUTPUT);
   pinMode(enB, OUTPUT);
 }
-void loop() {
-  mfront();
-  delay(2000);
-  mstop();
-  delay(500);
-  mback();
-  delay(2000);
-  mstop();
-  delay(500);
+void loop()
+{
+  box_detected = detect_box();
+  if (box_detected == true)
+  {
+    if (check_color(0))
+    {
+      if (measure_distance() <= distance_threshold)
+      {
+        mstop();
+        pick();
+      }
+      else
+      {
+        forward_control(255, 255);
+      }
+    }
+    else
+    {
+      forward_control(100, 255); // Going forward and turning
+    }
   }
-void mstop() { // stop motor
+  else
+  {
+    forward_control(100, 255); // Going forward and turning
+  }
+}
+void mstop()
+{ // stop motor
   analogWrite(enA, 0);
   analogWrite(enB, 0);
   digitalWrite(In1, LOW);
@@ -34,23 +55,56 @@ void mstop() { // stop motor
   digitalWrite(In3, LOW);
   digitalWrite(In4, LOW);
 }
-void mfront() { // motor go front
+void forward_control(int right_speed, int left_speed)
+{ // motor go front
 
   // Speed control
-  analogWrite(enA, 255); // max speed
-  analogWrite(enB, 255); // max speed
-  
-  
-  digitalWrite(In1, HIGH);
+  analogWrite(enA, right_speed); // max speed
+  analogWrite(enB, left_speed);  // max speed
+
+  digitalWrite(In1, LOW);
   digitalWrite(In2, LOW);
   digitalWrite(In3, HIGH);
-  digitalWrite(In4, LOW);
+  digitalWrite(In4, HIGH);
 }
-void mback() { // motor go back
-  analogWrite(enA, 255); // max speed
-  analogWrite(enB, 255); // max speed
-  digitalWrite(In1, LOW);
+void backward_control(int right_speed, int left_speed)
+{                                // motor go back
+  analogWrite(enA, right_speed); // max speed
+  analogWrite(enB, left_speed);  // max speed
+  digitalWrite(In1, HIGH);
   digitalWrite(In2, HIGH);
   digitalWrite(In3, LOW);
-  digitalWrite(In4, HIGH);
+  digitalWrite(In4, LOW);
+}
+
+void turn_right()
+{
+}
+
+bool check_color(int color)
+{
+  if (color == 0 || color == 1)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+int measure_distance()
+{
+  // Mesaure the distance using ultrasonic sensor
+  int distance = 10;
+  return distance;
+}
+
+void pick()
+{
+}
+
+bool detect_box()
+{
+  return true;
 }
